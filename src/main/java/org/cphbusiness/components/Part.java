b4632d5de0c34c5e01b44d7b0567375cf4e7240c
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-01-10 17:22:32 +0100

    Messing around with database

--- src/main/java/org/cphbusiness/components/Part.java
+++ src/main/java/org/cphbusiness/components/Part.java
@@ -4,6 +4,7 @@ public abstract class Part {
     private String name;
     private int quantity;
 
+
     public Part(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
