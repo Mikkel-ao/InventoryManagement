7514bdca399d66a3789285138151cf67f00d8ef7
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-01-10 17:55:08 +0100

    Added a mapper class.

--- src/main/java/org/cphbusiness/client/InventoryManager.java
+++ src/main/java/org/cphbusiness/client/InventoryManager.java
@@ -4,6 +4,7 @@ import org.cphbusiness.components.Part;
 
 import java.util.ArrayList;
 
+// TODO: Figure out how I want this interact the program. Client class that class upon mapper class methods?
 public class InventoryManager {
     private ArrayList<Part> parts;
 
---
+++ src/main/java/org/cphbusiness/database/InventoryMapper.java
@@ -0,0 +1,6 @@
+package org.cphbusiness.database;
+
+
+public class InventoryMapper {
+    
+}
