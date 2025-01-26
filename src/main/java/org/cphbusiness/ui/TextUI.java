1e10663a9b6fef5383b41444e5251c1993fb23a1
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-01-26 01:02:58 +0100

    display options method. Stay or move to client?

--- src/main/java/org/cphbusiness/ui/TextUI.java
+++ src/main/java/org/cphbusiness/ui/TextUI.java
@@ -1,5 +1,7 @@
 package org.cphbusiness.ui;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TextUI {
@@ -28,5 +30,42 @@ public class TextUI {
         System.out.println(text);
         String input = scanner.nextLine();
         return input;
+    }
+
+    public void displayMenu(){
+        List<String> options = new ArrayList<>();
+        System.out.println("What would you like to do?");
+
+        options.add("1. Display all parts");
+        options.add("2. Search for a part");
+        options.add("3. Add a new part");
+        options.add("4. Exit");
+
+        for(String option : options){
+            System.out.println(option);
+        }
+
+        int choice = promptNumeric("Choose an option by entering a number: ");
+
+        // TODO: Dummy souts. Revisit when methods are implemented
+        switch(choice) {
+            case 1:
+                System.out.println("Display all parts");
+                break;
+            case 2:
+                System.out.println("Search for a part");
+                break;
+            case 3:
+                System.out.println("Add a new part");
+                break;
+                // TODO: Option to save or discard changes?
+            case 4:
+                System.out.println("Exiting program...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Please enter a valid option. Pick a number from 1 to 4");
+
+        }
     }
 }
